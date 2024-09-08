11dfbf51cd2bd35ed235741a837c06fba779baa9
Belle <bellesee1212@gmail.com>
2024-09-07 20:43:25 -0400

    updated styling

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const [fact, setFact] = useState("");
-  
+
   useEffect(() => {
     async function getData() {
       const response = await fetch("https://dog-api.kinduff.com/api/facts");
@@ -16,39 +16,60 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 relative">
-      <div className="absolute bottom-2 left-2 pb-4 w-full">
-        <p className="font-gaegu text-center w-[90vw] m-auto">Did you know? {fact}</p>
-      </div>
-      <div className="absolute top-2 right-2">
-        <RSVP />
-      </div>
-      <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true"></link>
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossorigin
-      ></link>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Darumadrop+One&family=Gaegu&family=Roboto:wght@300;400;500;700&display=swap"
-        rel="stylesheet"
-      ></link>
-      <div>
-        <h1 className="font-darum text-3xl text-darkYellow text-center pb-3">
-          UW Student Wellness Office: Therapy puppies
-        </h1>
-        <p className="font-gaegu text-center text-xl max-w-md m-auto">
-          Come interact with puppies as a study break. The event is completely free for students. 
-        </p>
-        <p className="font-gaegu text-center text-xl max-w-md m-auto mt-3">
-          When? December 12th from 1-5pm 
-        </p>
-        <p className="font-gaegu text-center text-xl max-w-md m-auto">
-          Where? Student life center
-        </p>
-        <img src="/dog.png" className="max-w-md w-[90vw] my-3 m-auto" />
-        <RSVP />
-      </div>
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center px-24 relative">
+        <div className="hidden md:block absolute top-2 right-2">
+          <RSVP />
+        </div>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+          crossOrigin="true"
+        ></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Darumadrop+One&family=Gaegu&family=Roboto:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+        <div className="grid md:grid-cols-2 gap-3 py-6">
+          <div className="flex justify-center items-center">
+            <div>
+              <h1 className="font-darum text-2xl md:text-3xl text-darkYellow pb-3">
+                Paws for Wellness
+              </h1>
+              <p className="font-gaegu text-lg md:text-xl max-w-lg m-auto">
+                Come destress and play with our adorable therapy dogs. Research
+                shows it lowers your stress hormones, Cortisol, and increases
+                your feel-good hormones, Oxytocin. Also, it's dogs... you
+                probably don't need more convincing but in case you do, it's
+                also free!
+              </p>
+              <p className="font-gaegu text-lg md:text-xl max-w-lg m-auto mt-3">
+                ⏰ When? December 12th, 1-5pm
+              </p>
+              <p className="font-gaegu text-lg md:text-xl max-w-lg m-auto">
+                📍 Where? Student life center
+              </p>
+              <RSVP />
+            </div>
+          </div>
+
+          <img
+            src="/dog.jpg"
+            className="max-w-lg w-full rounded-md my-3 m-auto"
+          />
+        </div>
+
+        <div className="relative py-6 w-[90vw] max-w-2xl">
+          <p className="font-gaegu text-center text-darkYellow">
+            🤔 Did you know? {fact}
+          </p>
+        </div>
+      </main>
+    </>
   );
 }
